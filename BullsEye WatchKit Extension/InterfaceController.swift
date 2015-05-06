2c7f9bc6b0b9c61cb3830efdc25327e528437734
Super Liu <s5s5cn@gmail.com>
2015-05-06 19:22:40 +0800

    添加WATCH版本

---
+++ BullsEye WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,32 @@
+//
+//  InterfaceController.swift
+//  BullsEye WatchKit Extension
+//
+//  Created by s5s5 on 15/5/6.
+//  Copyright (c) 2015年 s5s5. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+    
+
+}
