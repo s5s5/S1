4bcec56f6054380397f12e415d238a37c708996f
Super Liu <s5s5cn@gmail.com>
2015-05-11 17:21:17 +0800

    每次都随机一个新指标

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -10,28 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
     var currentValue: Int = 0
+    var targetValue: Int = 0
+    
+    @IBOutlet weak var slider: UISlider!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+//        currentValue = lroundf(slider.value)
+//        targetValue = 1 + Int(arc4random_uniform(100))
+        startNewRound()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func showAlert() {
+        let message = "The value of the slider is: \(currentValue)"
+                    + "\nThe target value is: \(targetValue)"
+
+        let alert = UIAlertController(title: "Hello, World",
+                message: message, preferredStyle: .Alert)
+
+        let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
+
+        alert.addAction(action)
+
+        presentViewController(alert, animated: true, completion: nil)
+        
+        startNewRound()
+    }
+
+    @IBAction func sliderMoved(slider: UISlider) {
+        currentValue = lroundf(slider.value)
     }
     
-    @IBAction func showAlert() {
-        let alert = UIAlertController(title: "Hello, World", message: "This is my first app", preferredStyle: .Alert)
-        let action = UIAlertAction(title: "Awesome", style: .Default, handler: nil)
-        alert.addAction(action)
-        presentViewController(alert, animated: true, completion: nil)
+    func startNewRound() {
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 1 + Int(arc4random_uniform(100))
+        slider.value = Float(currentValue);
     }
 
-    @IBAction func sliderMoved(slider: UISlider){
-        currentValue = lroundf(slider.value)
-        
-    }
 
 }
 
